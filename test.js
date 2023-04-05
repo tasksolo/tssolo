2ddc2d2b0f012ce49fdd16f7d63e3f77cb33e561
Ian Gulliver <ian@solotask.io>
2023-04-05 03:25:03 +0000

    Sketchy test runner

--- test.js
+++ test.js
@@ -1,6 +1,21 @@
 import { strict as assert } from 'node:assert';
 import { Client } from './client.js';
 
-const client = new Client('https://api.solø.com/v1/');
-const dbg = await client.debugInfo();
-assert.ok(dbg)
+const tests = [
+	async () => {
+		const client = new Client('https://api.solø.com/v1/');
+		const dbg = await client.debugInfo();
+		assert.ok(dbg);
+	},
+];
+
+async function runTests() {
+	const proms = [];
+	for (const test of tests) {
+		proms.push(test());
+	}
+
+	return Promise.all(proms);
+}
+
+await runTests();
